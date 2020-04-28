098a008be5d515799909aa51d1b41ec84770fcfa
Saikat Saha <64367664+Saikatsaha798@users.noreply.github.com>
2020-04-28 21:32:52 +0530

    This the best far most working version as I tested.
    I found no errors in this but if any one finds one contact me. This version is the ultimate of the version I have created.

--- electronic config.py
+++ electronic config.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import sympy as sp
 from mendeleev import *
 
@@ -7,6 +9,7 @@ o = input('Enter the Symbol of element : ')
 o = element(o)
 z = o.atomic_number
 
+r = 0
 e = z
 n = 1
 econ = list()
@@ -19,22 +22,96 @@ c = {0: {'s': 2},
      5: {'h': 22},
      6: {'i': 26}}
 
-while e != 0:
-    for l in range(n):
-        u = list(c[l].keys())
-        if e > c[l][u[0]]:
-            sb = sp.symbols(u[0])
-            expr = n * sb ** c[l][u[0]]
-            econ.append(expr)
-            e -= c[l][u[0]]
-        elif e < c[l][u[0]]:
-            sb = sp.symbols(u[0])
-            expr = n * sb ** e
-            econ.append(expr)
+k = list()
+
+for n in range(1, 8):
+    for j in range(n):
+        a = n + j
+        sb = list(c[j].keys())
+        g = c[j][sb[0]]
+        k.append([a, sb[0], n, g])
+
+while e > 0:
+    for j in range(n):
+        if e == 0:
+            break
+        elif len(k) == 0:
+            break
+        elif e < k[0][3]:
+            if len(k) == 1:
+                u = sp.symbols(k[0][1])
+                v = k[0][2] * u ** e
+                econ.append(v)
+                k.pop(0)
+            elif k[0][0] < k[1][0]:
+                u = sp.symbols(k[0][1])
+                v = k[0][2] * u ** e
+                econ.append(v)
+                k.pop(0)
+            elif k[0][0] > k[1][0]:
+                u = sp.symbols(k[1][1])
+                v = k[1][2] * u ** e
+                econ.append(v)
+                k.pop(1)
+            else:
+                if k[0][2] < k[1][2]:
+                    u = sp.symbols(k[0][1])
+                    v = k[0][2] * u ** e
+                    econ.append(v)
+                    k.pop(0)
+                else:
+                    u = sp.symbols(k[1][1])
+                    v = k[1][2] * u ** e
+                    econ.append(v)
+                    k.pop(1)
             e = 0
             break
-        else:
+        elif len(k) == 1:
+            u = sp.symbols(k[0][1])
+            r = k[0][3]
+            v = k[0][2] * u ** k[0][3]
+            econ.append(v)
+            k.pop(0)
+            e = 0
             break
+        elif k[0][0] < k[1][0]:
+            r = k[0][3]
+            u = sp.symbols(k[0][1])
+            v = k[0][2] * u ** k[0][3]
+            econ.append(v)
+            e -= k[0][3]
+            k.pop(0)
+        elif k[0][0] > k[1][0]:
+            r = k[1][3]
+            u = sp.symbols(k[1][1])
+            v = k[1][2] * u ** k[1][3]
+            econ.append(v)
+            e -= k[1][3]
+            k.pop(1)
+        else:
+            if k[0][2] < k[1][2]:
+                r = k[0][3]
+                u = sp.symbols(k[0][1])
+                v = k[0][2] * u ** k[0][3]
+                econ.append(v)
+                e -= k[0][3]
+                k.pop(0)
+            else:
+                r = k[1][3]
+                u = sp.symbols(k[1][1])
+                v = k[1][2] * u ** k[1][3]
+                econ.append(v)
+                e -= k[1][3]
+                k.pop(1)
     n += 1
+
+if z == 24 or 29:
+    s = sp.symbols('s')
+    p = sp.symbols('p')
+    d = sp.symbols('d')
+    if z == 24:
+        econ = [s ** 2, 2 * s ** 2, 2 * p ** 6, 3 * s ** 2, 3 * p ** 6, 4 * s ** 1, 3 * d ** 5]
+    if z == 29:
+        econ = [s ** 2, 2 * s ** 2, 2 * p ** 6, 3 * s ** 2, 3 * p ** 6, 4 * s ** 1, 3 * d ** 10]
 
 print('Electronic Configuration of', z, ',', o.name, 'is : ', econ)
