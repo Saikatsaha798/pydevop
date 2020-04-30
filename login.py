a55372076eb48417a3976bb950e3f016511670a1
Saikat Saha <64367664+Saikatsaha798@users.noreply.github.com>
2020-04-30 11:39:37 +0530

    This has a basic level encryption.
    This program store the data in an encrption though it is not so standard but basic.

--- login.py
+++ login.py
@@ -1,51 +1,60 @@
 # to import os function
 import os
+import pickle
 
 print("Welcome, to ABC Programme \n 1. Sign In \n 2. Sign Up")
 
 # to get the list of directories.
-l=os.listdir()
+l = os.listdir()
 
 # to check whether the user file is present in the directory or not.
-if 'user.txt' in l:
-    c=int(input('Enter Your Choice : '))
-    if c==2:
-        u=input('Enter username')
-        p=input('Enter Password')
-        f=open("user.txt",'a')
-        f.write(u+','+p+',')
-        f.close()
-        print('Thank you, for signing up to login restart the programme ...:)')
-    if c==1:
-        u = input('Enter username')
-        p = input('Enter Password')
-        f=open('user.txt','r')
-        g=f.read()
-        t=g.split(',')
-        f.close()
-        if u and p in t:
-            print('Welcome', u, 'to ABC Programme')
-        else:
-            print('Incorrect Username and Password !!!!!')
-else:
-    m=open('user.txt','w')
-    m.close()
+if 'user.dat' in l:
     c = int(input('Enter Your Choice : '))
     if c == 2:
         u = input('Enter username')
         p = input('Enter Password')
-        f = open("user.txt", 'a')
-        f.write(u + ',' + p + ',')
+        f = open("user.dat", 'ab')
+        k = [u, p]
+        pickle.dump(k, f)
         f.close()
         print('Thank you, for signing up to login restart the programme ...:)')
     if c == 1:
         u = input('Enter username')
         p = input('Enter Password')
-        f = open('user.txt', 'r')
-        g = f.read()
-        t = g.split(',')
+        f = open('user.dat', 'rb')
+        g = pickle.load(f)
         f.close()
-    if u and p in t:
-        print('Welcome', u, 'to ABC Programme')
-    else:
-        print('Incorrect Username and Password !!!!!')
+        if u in g:
+            i = g.index(u)
+            if p == g[i + 1]:
+                print('Welcome', u, 'to ABC Programme')
+            else:
+                print('Incorrect Username and Password !!!!!')
+        else:
+            print('Incorrect Username and Password !!!!!')
+else:
+    m = open('user.dat', 'wb')
+    m.close()
+    c = int(input('Enter Your Choice : '))
+    if c == 2:
+        u = input('Enter username')
+        p = input('Enter Password')
+        f = open("user.dat", 'wb')
+        k = [u, p]
+        pickle.dump(k, f)
+        f.close()
+        print('Thank you, for signing up to login restart the programme ...:)')
+    if c == 1:
+        u = input('Enter username')
+        p = input('Enter Password')
+        f = open('user.dat', 'rb')
+        g = pickle.load(f)
+        f.close()
+        if u in g:
+            i = g.index(u)
+            if p == g[u + 1]:
+                print('Welcome', u, 'to ABC Programme')
+            else:
+                print('Incorrect Username and Password !!!!!')
+        else:
+            print('Incorrect Username and Password !!!!!')
