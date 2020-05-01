8f3d4cd82f3b12f54a8587ef59bfffc1ac683a42
Saikat Saha <64367664+Saikatsaha798@users.noreply.github.com>
2020-05-01 18:05:22 +0530

    This is the random group creator an very versatile.
    This is created to choose member of groups randomly.

---
+++ Random Group Creator.py
@@ -0,0 +1,30 @@
+import random
+
+n = int(input('Enter the Number of Names to be Entered : '))
+p = int(input('Enter the number of Participants allowed in a group : '))
+
+m = list()
+o = list()
+v = 0
+
+f = open('output.txt', 'a')
+
+for i in range(n):
+    a = input('Enter the name : ')
+    m.append(a)
+
+random.shuffle(m)
+
+while n // p != v:
+    o.clear()
+    w = random.choices(m, k=p)
+    for y in w:
+        if m.count(y) == 1:
+            o.append(y)
+            m.remove(y)
+        else:
+            break
+    if len(o) == p:
+        print(o)
+        f.write(str(o))
+        v += 1
